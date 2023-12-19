b89ecf3e1bd82c42037adcb9ff4211dbc23882fc
Laureano73 <laureanotrilo@hotmail.com>
2023-12-19 17:48:35 -0300

    primer desafio despues de la primera entrega

--- pre entrega 1/src/app.js
+++ pre entrega 1/src/app.js
@@ -1,4 +1,8 @@
 const express = require('express');
+const exphbs = require('express-handlebars');
+const http = require('http');
+const { Server } = require('socket.io');
+const path = require('path');
 const ProductManager = require('./ProductManager');
 const CartManager = require('./CartManager');
 const productsRouter = require('./routes/products');
@@ -14,36 +18,97 @@ app.use(express.json());
 
 // Función para inicializar productos
 async function initializeProducts() {
+    // ... lógica para inicializar productos ...
+}
+
+// Función para obtener y filtrar productos
+async function getFilteredProducts() {
     try {
-        // Agrega productos al iniciar la aplicación
-        await productManager.addProduct({
-            title: 'Producto 1',
-            description: 'Descripción 1',
-            price: 20,
-            thumbnail: 'imagen 1',
-            code: 'P1',
-            stock: 5,
-        });
+        const products = await productManager.getProducts();
 
-        await productManager.addProduct({
-            title: 'Producto 2',
-            description: 'Descripción 2',
-            price: 30,
-            thumbnail: 'imagen 2',
-            code: 'P2',
-            stock: 8,
-        });
-
-        console.log('Productos inicializados.');
+        // Filtra los productos para asegurarte de que tengan las propiedades necesarias
+        return products.filter(product => product.id && product.title && product.description && product.price);
     } catch (error) {
-        console.error('Error al inicializar productos:', error);
+        console.error('Error al obtener la lista de productos:', error);
+        throw new Error('Error al obtener la lista de productos');
     }
 }
 
 app.use('/api/products', productsRouter);
 app.use('/api/carts', cartsRouter);
 
-app.listen(port, async () => {
+// Configuración de Handlebars
+const hbs = exphbs.create();
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+app.set('views', path.join(__dirname, 'views'));
+
+// Crear el servidor HTTP
+const server = http.createServer(app);
+
+// Configuración de Socket.io
+const io = new Server(server);
+
+io.on('connection', (socket) => {
+    console.log('Usuario conectado');
+
+    socket.on('disconnect', () => {
+        console.log('Usuario desconectado');
+    });
+});
+
+app.get('/', async (req, res) => {
+    console.log('Accediendo a la página principal');
+
+    try {
+        const products = await getFilteredProducts();
+        console.log('Products:', products);
+
+        res.render('layouts/main', { title: 'Página Principal', products });
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
+});
+
+app.get('/realtimeproducts', async (req, res) => {
+    try {
+        const products = await getFilteredProducts();
+        console.log('Filtered Products:', products);
+
+        res.render('realtimeproducts', { products });
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
+});
+
+app.post('/api/products', async (req, res) => {
+    try {
+        const newProduct = await productManager.addProduct(req.body);
+
+        // Emite el evento de producto agregado a todos los clientes conectados
+        io.emit('productAdded', { product: newProduct });
+
+        res.json(newProduct);
+    } catch (error) {
+        res.status(500).json({ error: 'Error al agregar el producto' });
+    }
+});
+
+app.delete('/api/products/:id', async (req, res) => {
+    const productId = req.params.id;
+    try {
+        await productManager.deleteProduct(productId);
+
+        // Emite el evento de producto eliminado a todos los clientes conectados
+        io.emit('productDeleted', { productId });
+
+        res.json({ success: true });
+    } catch (error) {
+        res.status(500).json({ error: 'Error al eliminar el producto' });
+    }
+});
+
+server.listen(port, async () => {
     try {
         console.log(`Servidor escuchando en http://localhost:${port}`);
         await initializeProducts();
